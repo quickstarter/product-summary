2cc66d2e7167cd182a22bbfcb88e06d8053069e1
CamFields <CameronFielder@gmail.com>
2018-04-17 00:41:28 -0700

    fs changes, api calls

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,2 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import sampleData from '../../db/sampleData'
+
+class App extends React.Component {
+  render() {
+    console.log(sampleData[0].mainDisplay)
+    return (
+        <h1>{sampleData[0].mainDisplay.title}</h1>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
