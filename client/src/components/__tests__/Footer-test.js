d9cb67744d99fbeffa0957782c13f00ffee239b9
CamFields <CameronFielder@gmail.com>
2018-04-26 12:17:25 -0700

    testing

---
+++ client/src/components/__tests__/Footer-test.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { shallow, mount, render } from 'enzyme';
+import renderer from 'react-test-renderer';
+import Footer from '../Footer.js';
---
+++ client/src/components/__tests__/MainImage-test.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { shallow, mount, render } from 'enzyme';
+import renderer from 'react-test-renderer';
+import MainImage from '../MainImage.js';
